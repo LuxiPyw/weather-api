56b6d25d00b778ebb0403159a88e93f3e0ed5fb4
LuxiPyw <luxipew@gmail.com>
2024-10-08 19:53:04 +0300

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Diagnostics.CodeAnalysis;
-using Test;
 using System.Security;
 using System.Text;
 using System.Runtime.InteropServices.JavaScript;
@@ -59,7 +58,7 @@ namespace API
             public float lon { get; set; }
             public string tz_id { get; set; }
             public int localtime_epoch { get; set; }
-            public string localtime	{ get; set; }
+            public string localtime { get; set; }
         }
     }
 
@@ -75,45 +74,64 @@ namespace API
 
             //Это уже работает и выдает json файл но нужно думать с тем что не всегда дает ответ 
 
-            try
+            while (true)
             {
-
-
-
-                using HttpResponseMessage response = await client.GetAsync("http://api.weatherapi.com/v1/current.json?key=1a2abfabfd5445b588a200307242609&q=St Petersburg");
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseBody);
-
-                string path = @"c:\temp\MyTest.json";
-                using (FileStream fs = File.Create(path))
+                try
                 {
-                    AddText(fs, responseBody);
+                    System.Console.WriteLine("Write a lat, lon/ name city \n Example: 51.5171 -0.1063 \t London/St Petersburg \n\t If you want to close a window write <exit> ");
+                    string plus = System.Console.ReadLine();
+                    if (plus == "exit"){
+                        break;
+                    }
+                    string Reserch = "http://api.weatherapi.com/v1/current.json?key=1a2abfabfd5445b588a200307242609&q=";
+                    using HttpResponseMessage response = await client.GetAsync(Reserch + plus);
+                    //using HttpResponseMessage response = await client.GetAsync("http://api.weatherapi.com/v1/current.json?key=1a2abfabfd5445b588a200307242609&q=St Petersburg");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    //Console.WriteLine(responseBody);
+
+                    /*   string path = @"c:\temp\settings.txt";
+                       using (FileStream fs = File.Create(path))
+                       {
+                           AddText(fs, responseBody);
+                       }
+                       using FileStream openStream = File.OpenRead(path);*/
+
+
+                    RootObject? root = JsonSerializer.Deserialize<RootObject>(responseBody);
+                    System.Console.WriteLine("output:");
+
+
+                    //WeatherResponse real_response = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+                    //System.Console.WriteLine(real_response.temp);
+                    //Хз как надо конвертировать json в уже читабельный вид но это задача на затра сейчас уже 11:20PM
+
+                    System.Console.WriteLine($"Name:{root.location.name}");
+                    System.Console.WriteLine($"region:{root.location.region}");
+                    System.Console.WriteLine($"lat:{root.location.lat}");
+                    System.Console.WriteLine($"lon:{root.location.lon}");
+                    System.Console.WriteLine($"Time:{root.location.localtime}\n");
+                    System.Console.WriteLine($"Temp per C:{root.current.temp_c} ");
+                    System.Console.WriteLine($"Day:{root.current.is_day}");
+                    System.Console.WriteLine($"Precip mm:{root.current.precip_mm}");
+                    System.Console.WriteLine($"Cloud:{root.current.cloud}");
+                    System.Console.WriteLine($"Vis km:{root.current.vis_km}");
+                    System.Console.WriteLine($"Gust kp/h:{root.current.gust_kph}");
+
+                    System.Console.ReadKey();
+                    Console.Clear();
+
                 }
-
-
-                using FileStream openStream = File.OpenRead(path);
-                RootObject? root = JsonSerializer.Deserialize<RootObject>(responseBody);
-                System.Console.WriteLine("output:");
-
-
-                //WeatherResponse real_response = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
-                //System.Console.WriteLine(real_response.temp);
-                //Хз как надо конвертировать json в уже читабельный вид но это задача на затра сейчас уже 11:20PM
-
-
-                System.Console.WriteLine($"Temp:{root.current.temp_c} ");
-
-                System.Console.ReadKey();
-
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine(ex.Message);
+                    System.Console.WriteLine("Press ENTER to continue");
+                    System.Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
             }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
-                System.Console.ReadLine();
-            }
-
         }
 
         private static void AddText(FileStream fs, string value)
